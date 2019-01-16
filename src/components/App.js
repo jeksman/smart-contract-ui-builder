8868789a59bc6399ca9d85b5286a48165effe8ba
Eugene Kyselev <admin@profsite.com.ua>
2019-01-16 12:38:26 +0200

    change middleware to redux-thunk, rendering issues persist

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,60 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+// import Web3 from 'web3'
+
 import Grapher from './Grapher'
-import './App.css';
+import Header from './Header'
+import './App.css'
+
+import { getWeb3 } from '../redux/web3'
 
 class App extends Component {
-  render() {
+
+  constructor (props) {
+    super(props)
+    this.props.getWeb3()
+  }
+
+  render () {
+    console.log('App render', this.props)
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Dapp Grapher</h1>
-        </header>
+        <Header version={this.props.web3 ? this.props.web3.version : 'nil'}/>
         <Grapher />
       </div>
-    );
+    )
   }
 }
 
-export default App
+App.propTypes = {
+  web3: PropTypes.object,
+  getWeb3: PropTypes.function,
+}
+
+const mapStateToProps = state => {
+  console.log('mapStateToProps', state)
+  return {
+    web3: state.web3,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getWeb3: () => dispatch(getWeb3()),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
+
+// export default connect(
+//   state => ({
+//     web3: state.web3,
+//   }),
+//   dispatch => ({
+//     getWeb3: () => dispatch(getWeb3Action()),
+//   })
+// )(App)
