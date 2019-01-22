1d316ecb33570f6fe81c06f3cc64caf924760b9a
Eugene Kyselev <admin@profsite.com.ua>
2019-01-22 08:27:05 +0200

    fix EIP1102-compliance bug

--- src/components/Web3Gatekeeper.js
+++ src/components/Web3Gatekeeper.js
@@ -8,19 +8,19 @@ import Button from '@material-ui/core/Button'
 import EthereumIcon from './common/EthereumIcon'
 
 const containerStyle = {
-  position: 'absolute',
-  width: '100%',
-  top: '50%',
-  display: 'flex',
-  justifyContent: 'center',
-  flexDirection: 'column',
+    position: 'absolute',
+    width: '100%',
+    top: '50%',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
 }
 
 const buttonStyle = {
-  top: '15px',
-  alignSelf: 'center',
-  color: '#ffffff',
-  backgroundColor: '#4527a0', // same as app header
+    top: '15px',
+    alignSelf: 'center',
+    color: '#ffffff',
+    backgroundColor: '#4527a0', // same as app header
 }
 
 /**
@@ -32,99 +32,99 @@ const buttonStyle = {
  */
 export default class Web3Gatekeeper extends Component {
 
-  constructor (props) {
+    constructor (props) {
 
-    super(props)
+        super(props)
 
-    this.state = {
-      ethereumInjected: false,
-      web3Enabled: false,
-      enableFailed: false,
-    }
-  }
-
-  componentDidMount () {
-
-    // only allow MetaMask for now
-    // TODO: let other dapp browsers through
-    if (window.ethereum && window.ethereum.isMetaMask) {
-      this.setState({ ethereumInjected: true })
+        this.state = {
+            ethereumInjected: false,
+            web3Enabled: false,
+            enableFailed: false,
+        }
     }
 
-    // if there's a selected address available, the user has already enabled
-    // web3
-    if (window.ethereum.selectedAddress) {
-      this.setState({ web3Enabled: true })
-    }
-  }
+    componentDidMount () {
 
-  render () {
+        // only allow MetaMask for now
+        // TODO: let other dapp browsers through
+        if (window.ethereum && window.ethereum.isMetaMask) {
+            this.setState({ ethereumInjected: true })
+        }
 
-    if (!this.state.web3Enabled) {
-
-      return (
-        <Fragment>
-          <EthereumIcon/>
-          <div style={containerStyle}>
-            <Typography
-              align="center"
-              variant="display3"
-            >
-              {
-                !this.state.ethereumInjected
-                ? 'Please Download MetaMask'
-                : this.state.enableFailed
-                  ? 'Access Denied'
-                  : 'Please Connect to MetaMask'
-              }
-            </Typography>
-            {
-              this.state.enableFailed || !this.state.ethereumInjected
-              ? null
-              : <Button
-                  size="large"
-                  variant="extendedFab"
-                  style={buttonStyle}
-                  onClick={this.handleConnect}
-                >
-                  Connect
-                </Button>
-            }
-          </div>
-        </Fragment>
-      )
+        // if there's a selected address available, the user has already enabled
+        // web3
+        if (window.ethereum && window.ethereum.selectedAddress) {
+            this.setState({ web3Enabled: true })
+        }
     }
 
-    return this.props.children
-  }
+    render () {
 
-  /**
-   * Calls window.ethereum.enable() and sets component state per result.
-   */
-  handleConnect = async (event) => {
+        if (!this.state.web3Enabled) {
 
-    event.preventDefault()
+            return (
+                <Fragment>
+                    <EthereumIcon/>
+                    <div style={containerStyle}>
+                        <Typography
+                            align="center"
+                            variant="display3"
+                        >
+                            {
+                                !this.state.ethereumInjected
+                                    ? 'Please Download MetaMask'
+                                    : this.state.enableFailed
+                                    ? 'Access Denied'
+                                    : 'Please Connect to MetaMask'
+                            }
+                        </Typography>
+                        {
+                            this.state.enableFailed || !this.state.ethereumInjected
+                                ? null
+                                : <Button
+                                    size="large"
+                                    variant="extendedFab"
+                                    style={buttonStyle}
+                                    onClick={this.handleConnect}
+                                >
+                                    Connect
+                                </Button>
+                        }
+                    </div>
+                </Fragment>
+            )
+        }
 
-    let accounts
-
-    try {
-      accounts = await window.ethereum.enable()
-    } catch (error) {
-      console.log(error)
-      this.setState({ enableFailed: true })
-      return
+        return this.props.children
     }
 
-    // since assuming MetaMask, exactly one account should be returned
-    if (accounts.length !== 1) {
-      console.warn('More than one Web3 account found. ' +
-        'Defaulting to first account.', accounts)
-    }
+    /**
+     * Calls window.ethereum.enable() and sets component state per result.
+     */
+    handleConnect = async (event) => {
 
-    this.setState({ web3Enabled: true })
-  }
+        event.preventDefault()
+
+        let accounts
+
+        try {
+            accounts = await window.ethereum.enable()
+        } catch (error) {
+            console.log(error)
+            this.setState({ enableFailed: true })
+            return
+        }
+
+        // since assuming MetaMask, exactly one account should be returned
+        if (accounts.length !== 1) {
+            console.warn('More than one Web3 account found. ' +
+                'Defaulting to first account.', accounts)
+        }
+
+        this.setState({ web3Enabled: true })
+    }
 }
 
 Web3Gatekeeper.propTypes = {
-  children: PropTypes.object,
+    children: PropTypes.object,
 }
